7af37428d22fac00816470efb14788f35077a459
persistent-path-1995 <mwpenn94@gmail.com>
2020-12-01 17:05:53 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -1,7 +1,11 @@
 require_relative"../lib/display_board.rb"
 
 def display_board(board)
-  puts 
+  puts  " {board[0]} | {board[1]} | {board[2]} "
+   puts "-----------"
+   puts " {board[3]} | {board[4]} | {board[5]} "
+   puts "-----------"
+   puts " {board[6]} | {board[7]} | {board[8]} "
 end
 
 describe "#display_board in 'lib/display_board.rb" do
